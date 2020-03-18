da2b5ba1b62d8a59068e7880caa73f06d4a721d4
Masao Kitamura <masaok@gmail.com>
2020-03-17 20:10:29 -0700

    server side only RP works

--- test.js
+++ test.js
@@ -1,107 +1,178 @@
 //include required modules
-const jwt = require('jsonwebtoken');
-const config = require('./config');
-const rp = require('request-promise');
-const fetch = require("node-fetch");
+const jwt = require('jsonwebtoken')
+const config = require('./config')
+const rp = require('request-promise')
+const fetch = require('node-fetch')
 
-
-const express = require('express');
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-var email, userid, resp;
-const port = 3000;
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+var email, userid, resp
+const port = 3000
 
 //Use the ApiKey and APISecret from config.js
 const payload = {
-    iss: config.APIKey,
-    exp: ((new Date()).getTime() + 5000)
-};
-const token = jwt.sign(payload, config.APISecret);
-console.log("token: " + token)
+  iss: config.APIKey,
+  exp: new Date().getTime() + 5000
+}
+const token = jwt.sign(payload, config.APISecret)
+console.log('token: ' + token)
 
 const testPost = async () => {
-    console.log("testPost token: " + token)
-    const email = 'masaok@gmail.com'
-    const uri = "https://api.zoom.us/v2/users/"+email 
-    const settings = {
-        method: 'POST',
-        uri,
-        qs: {
-            status: 'active' 
-        },
-        auth: {
-            'bearer': token
-        },
-        headers: {
-            'User-Agent': 'Zoom-api-Jwt-Request',
-            'content-type': 'application/json'
-        },
-        json: true //Parse the JSON string in the response
-    };
-    try {
-        const fetchResponse = await fetch(uri, settings);
-        const data = await fetchResponse.json();
-        console.log(data)
-        return data;
-    } catch (e) {
-        return e;
-    }    
+  console.log('testPost token: ' + token)
+  const email = 'masaok@gmail.com'
+  const uri = 'https://api.zoom.us/v2/users/' + email
+  const settings = {
+    method: 'POST',
+    uri,
+    qs: {
+      status: 'active'
+    },
+    auth: {
+      bearer: token
+    },
+    headers: {
+      'User-Agent': 'Zoom-api-Jwt-Request',
+      'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
+  }
+  try {
+    const fetchResponse = await fetch(uri, settings)
+    const data = await fetchResponse.json()
+    console.log(data)
+    return data
+  } catch (e) {
+    return e
+  }
 }
 
 const result = testPost()
 console.log(result)
 
-
-//get the form 
-app.get('/', (req,res) => res.send(req.body));
+//get the form
+app.get('/', (req, res) => res.send(req.body))
 
 //use userinfo from the form and make a post request to /userinfo
 app.post('/userinfo', (req, res) => {
   //store the email address of the user in the email variable
-    email = req.body.email;
+  email = req.body.email
   //check if the email was stored in the console
-  console.log(email);
+  console.log(email)
   //Store the options for Zoom API which will be used to make an API call later.
   var options = {
     //You can use a different uri if you're making an API call to a different Zoom endpoint.
-    uri: "https://api.zoom.us/v2/users/"+email, 
+    uri: 'https://api.zoom.us/v2/users/' + email,
     qs: {
-        status: 'active' 
+      status: 'active'
     },
     auth: {
-        'bearer': token
+      bearer: token
     },
     headers: {
-        'User-Agent': 'Zoom-api-Jwt-Request',
-        'content-type': 'application/json'
+      'User-Agent': 'Zoom-api-Jwt-Request',
+      'content-type': 'application/json'
     },
     json: true //Parse the JSON string in the response
-};
+  }
 
-//Use request-promise module's .then() method to make request calls.
-rp(options)
-    .then(function (response) {
+  //Use request-promise module's .then() method to make request calls.
+  rp(options)
+    .then(function(response) {
       //printing the response on the console
-        console.log('User has', response);
-        //console.log(typeof response);
-        resp = response
-        //Adding html to the page
-        var title1 ='<center><h3>Your token: </h3></center>' 
-        var result1 = title1 + '<code><pre style="background-color:#aef8f9;">' + token + '</pre></code>';
-        var title ='<center><h3>User\'s information:</h3></center>' 
-        //Prettify the JSON format using pre tag and JSON.stringify
-        var result = title + '<code><pre style="background-color:#aef8f9;">'+JSON.stringify(resp, null, 2)+ '</pre></code>'
-        res.send(result1 + '<br>' + result);
- 
+      console.log('User has', response)
+      //console.log(typeof response);
+      resp = response
+      //Adding html to the page
+      var title1 = '<center><h3>Your token: </h3></center>'
+      var result1 =
+        title1 +
+        '<code><pre style="background-color:#aef8f9;">' +
+        token +
+        '</pre></code>'
+      var title = "<center><h3>User's information:</h3></center>"
+      //Prettify the JSON format using pre tag and JSON.stringify
+      var result =
+        title +
+        '<code><pre style="background-color:#aef8f9;">' +
+        JSON.stringify(resp, null, 2) +
+        '</pre></code>'
+      res.send(result1 + '<br>' + result)
     })
-    .catch(function (err) {
-        // API call failed...
-        console.log('API call failed, reason ', err);
-    });
+    .catch(function(err) {
+      // API call failed...
+      console.log('API call failed, reason ', err)
+    })
+})
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-});
+email = 'masaok@gmail.com'
+const uri = 'https://api.zoom.us/v2/users/' + email
 
+var options = {
+  // method: 'POST',
+  uri,
+  qs: {
+    status: 'active'
+  },
+  auth: {
+    bearer: token
+  },
+  headers: {
+    'User-Agent': 'Zoom-api-Jwt-Request',
+    'content-type': 'application/json'
+  },
+  json: true //Parse the JSON string in the response
+}
+console.log('OPTIONS:')
+console.log(options)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+rp(options)
+  .then(function(body) {
+    // POST succeeded...
+    console.log('RP POST SUCCESS')
+    console.log(body)
+  })
+  .catch(function(err) {
+    // POST failed...
+    console.log('RP POST ERROR')
+    console.log(err)
+  })
+
+const main = async () => {
+  email = 'masaok@gmail.com'
+  const uri = 'https://api.zoom.us/v2/users/' + email
+
+  var options = {
+    // method: 'POST',
+    uri,
+    qs: {
+      status: 'active'
+    },
+    auth: {
+      bearer: token
+    },
+    headers: {
+      'User-Agent': 'Zoom-api-Jwt-Request',
+      'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
+  }
+  console.log('OPTIONS:')
+  console.log(options)
+
+  rp(options)
+    .then(function(body) {
+      // POST succeeded...
+      console.log('RP POST SUCCESS')
+      console.log(body)
+    })
+    .catch(function(err) {
+      // POST failed...
+      console.log('RP POST ERROR')
+      console.log(err)
+    })
+}
+main()
