ba957bee3802d3935ce6e8f423c7e75e8600927c
Masao Kitamura <masaok@gmail.com>
2020-03-17 19:39:05 -0700

    invalid token when not using the node server

--- config.sample.js
+++ config.sample.js
@@ -1,9 +1,6 @@
 const env = process.env.NODE_ENV || 'production'
 
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
-
-console.log(process.env)
-
 const config = {
 	development :{
 		APIKey : '',
---
+++ test.js
@@ -0,0 +1,107 @@
+//include required modules
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const rp = require('request-promise');
+const fetch = require("node-fetch");
+
+
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+var email, userid, resp;
+const port = 3000;
+
+//Use the ApiKey and APISecret from config.js
+const payload = {
+    iss: config.APIKey,
+    exp: ((new Date()).getTime() + 5000)
+};
+const token = jwt.sign(payload, config.APISecret);
+console.log("token: " + token)
+
+const testPost = async () => {
+    console.log("testPost token: " + token)
+    const email = 'masaok@gmail.com'
+    const uri = "https://api.zoom.us/v2/users/"+email 
+    const settings = {
+        method: 'POST',
+        uri,
+        qs: {
+            status: 'active' 
+        },
+        auth: {
+            'bearer': token
+        },
+        headers: {
+            'User-Agent': 'Zoom-api-Jwt-Request',
+            'content-type': 'application/json'
+        },
+        json: true //Parse the JSON string in the response
+    };
+    try {
+        const fetchResponse = await fetch(uri, settings);
+        const data = await fetchResponse.json();
+        console.log(data)
+        return data;
+    } catch (e) {
+        return e;
+    }    
+}
+
+const result = testPost()
+console.log(result)
+
+
+//get the form 
+app.get('/', (req,res) => res.send(req.body));
+
+//use userinfo from the form and make a post request to /userinfo
+app.post('/userinfo', (req, res) => {
+  //store the email address of the user in the email variable
+    email = req.body.email;
+  //check if the email was stored in the console
+  console.log(email);
+  //Store the options for Zoom API which will be used to make an API call later.
+  var options = {
+    //You can use a different uri if you're making an API call to a different Zoom endpoint.
+    uri: "https://api.zoom.us/v2/users/"+email, 
+    qs: {
+        status: 'active' 
+    },
+    auth: {
+        'bearer': token
+    },
+    headers: {
+        'User-Agent': 'Zoom-api-Jwt-Request',
+        'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
+};
+
+//Use request-promise module's .then() method to make request calls.
+rp(options)
+    .then(function (response) {
+      //printing the response on the console
+        console.log('User has', response);
+        //console.log(typeof response);
+        resp = response
+        //Adding html to the page
+        var title1 ='<center><h3>Your token: </h3></center>' 
+        var result1 = title1 + '<code><pre style="background-color:#aef8f9;">' + token + '</pre></code>';
+        var title ='<center><h3>User\'s information:</h3></center>' 
+        //Prettify the JSON format using pre tag and JSON.stringify
+        var result = title + '<code><pre style="background-color:#aef8f9;">'+JSON.stringify(resp, null, 2)+ '</pre></code>'
+        res.send(result1 + '<br>' + result);
+ 
+    })
+    .catch(function (err) {
+        // API call failed...
+        console.log('API call failed, reason ', err);
+    });
+
+
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
